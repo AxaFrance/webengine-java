1cf775f69e51219f648b1fbbc4437556ce7c736f
S636941 <S636941@PO701876.siege.axa-fr.intraxa>
2022-07-06 17:41:03 +0200

    Modify timeout

--- webengine-web/src/main/java/fr/axa/automation/webengine/general/SettingsWeb.java
+++ webengine-web/src/main/java/fr/axa/automation/webengine/general/SettingsWeb.java
@@ -1,6 +1,6 @@
 package fr.axa.automation.webengine.general;
 
 public interface SettingsWeb {
-    int TIMEOUT_SECONDES = 45;
+    int TIMEOUT_SECONDES = 60;
     long WAIT_TIME_MILLISECONDES = 1000L;
 }
