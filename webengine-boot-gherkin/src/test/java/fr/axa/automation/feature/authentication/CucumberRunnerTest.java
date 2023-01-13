5df2b17353dbe2168920153977f9e8eea95384b6
A247ZG <joseph.arul@axa.fr>
2023-01-13 14:22:30 +0100

    Remove Junit 4 - Full Junit 5 - Add test project for keyworddriven

--- webengine-boot-gherkin/src/test/java/fr/axa/automation/feature/authentication/CucumberRunnerTest.java
+++ webengine-boot-gherkin/src/test/java/fr/axa/automation/feature/authentication/CucumberRunnerTest.java
@@ -1,13 +1,9 @@
 package fr.axa.automation.feature.authentication;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.*;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(   features = "src/test/resources/features",
-                    glue = "fr.axa.automation.feature",
-                    plugin = {"pretty" , "html:target/report-gherkin/report.html","fr.axa.automation.webengine.listener.WebengineReportListener", "json:target/cucumber-report/cucumber.json"} )
+@Suite
+@IncludeEngines("cucumber")
 public class CucumberRunnerTest {
 
 }
