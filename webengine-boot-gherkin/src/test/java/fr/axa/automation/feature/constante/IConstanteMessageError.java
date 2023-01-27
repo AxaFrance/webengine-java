ffef4c74bbb0a79055d705c60a686269ce10085a
A247ZG <joseph.arul@axa.fr>
2023-01-27 09:50:31 +0100

    Move test gherkin in new module webengine-test-gherkin

--- webengine-boot-gherkin/src/test/java/fr/axa/automation/feature/constante/IConstanteMessageError.java
+++
@@ -1,6 +0,0 @@
-package fr.axa.automation.feature.constante;
-
-public interface IConstanteMessageError {
-    String AUTHENTICATION_ERROR_MESSAGE = "Error lors de l'authentication";
-    String AUTHENTICATION_ACCUEIL_ERROR_MESSAGE = "La page d'accueil n'est pas affiché";
-}
