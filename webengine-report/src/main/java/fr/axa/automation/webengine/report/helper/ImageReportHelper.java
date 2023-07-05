0c8ce7e4d8b9130f373e64d55dd318f9b0f9eced
A247ZG <joseph.arul@axa.fr>
2023-07-05 15:29:00 +0200

    update report html

--- webengine-report/src/main/java/fr/axa/automation/webengine/report/helper/ImageReportHelper.java
+++ webengine-report/src/main/java/fr/axa/automation/webengine/report/helper/ImageReportHelper.java
@@ -40,8 +40,10 @@ public final class ImageReportHelper {
     private static List<ScreenshotReport> getScreenShotReport(ActionReport actionReport) {
         List<ScreenshotReport> screenshotReportList = new ArrayList<>();
         List<ScreenshotReport> screenshotReportListOfSubReport = new ArrayList<>();
-        if(actionReport!=null && actionReport.getScreenshots()!=null && CollectionUtils.isNotEmpty(actionReport.getScreenshots().getScreenshotReports())){
-            screenshotReportList = actionReport.getScreenshots().getScreenshotReports().stream().collect(Collectors.toList());
+        if(actionReport != null){
+            if(actionReport.getScreenshots() != null && CollectionUtils.isNotEmpty(actionReport.getScreenshots().getScreenshotReports())){
+                screenshotReportList = actionReport.getScreenshots().getScreenshotReports().stream().collect(Collectors.toList());
+            }
             if(actionReport.getSubActionReports()!=null){
                 actionReport.getSubActionReports().getActionReports().stream().forEach(at -> screenshotReportListOfSubReport.addAll(getScreenShotReport(at)));
             }
