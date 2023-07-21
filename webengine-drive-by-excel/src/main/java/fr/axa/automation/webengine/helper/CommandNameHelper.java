3ef32b0608c71238655308fa17e7327fe4c4ca9a
A247ZG <joseph.arul@axa.fr>
2023-07-21 14:00:23 +0200

    Return command name if label is empty for the report

--- webengine-drive-by-excel/src/main/java/fr/axa/automation/webengine/helper/CommandNameHelper.java
+++ webengine-drive-by-excel/src/main/java/fr/axa/automation/webengine/helper/CommandNameHelper.java
@@ -1,10 +1,16 @@
 package fr.axa.automation.webengine.helper;
 
 import fr.axa.automation.webengine.object.CommandDataNoCode;
+import org.apache.commons.lang3.StringUtils;
 
 public class CommandNameHelper {
 
     public static String getCommandName(CommandDataNoCode commandData) {
-        return commandData.getName();
+        String name = commandData.getName();
+        if(StringUtils.isEmpty(name)){
+            name = commandData.getCommand().name();
+        }
+        return name;
+
     }
 }
