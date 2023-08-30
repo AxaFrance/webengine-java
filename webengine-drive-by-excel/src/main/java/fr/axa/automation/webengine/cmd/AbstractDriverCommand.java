c7f6d9e803000fcb1a64a3db2563d3600a9aead5
A247ZG <joseph.arul@axa.fr>
2023-08-30 09:50:08 +0200

    Fix bug when if command failed called by a call command. The result is failed

--- webengine-drive-by-excel/src/main/java/fr/axa/automation/webengine/cmd/AbstractDriverCommand.java
+++ webengine-drive-by-excel/src/main/java/fr/axa/automation/webengine/cmd/AbstractDriverCommand.java
@@ -163,7 +163,8 @@ public abstract class AbstractDriverCommand implements ICommand {
             actionReport.setLog(getLogReport().toString());
         } catch (Throwable e) {
             actionReport.getScreenshots().getScreenshotReports().add(screenShot(globalApplicationContext,testCaseContext,"",commandResultList));
-            if (commandData.isOptional()) {
+            if (commandData.isOptional() || commandData.getCommand() != CommandName.IF || commandData.getCommand() != CommandName.ELSE_IF) {
+                actionReport.setName(actionReport.getName() + " - /!\\ Failed but ignored (Optional or If/else if/else)");
                 actionReport.setResult(Result.IGNORED);
                 getLogReport().append(ConstantNoCode.CR_LF.getValue()).append("Warning : ").append(ConstantNoCode.CR_LF.getValue()).append(" Command failed but ignored because this command is optional ");
             }else{
@@ -182,6 +183,7 @@ public abstract class AbstractDriverCommand implements ICommand {
                 .webDriver(webDriver)
                 .savedData(savedData).build();
     }
+
 
     protected ScreenshotReport screenShot(AbstractGlobalApplicationContext globalApplicationContext,AbstractTestCaseContext testCaseContext,String name, List<CommandResult> commandResultList) throws WebEngineException {
         WebDriver webDriver = getWebDriverToUse(globalApplicationContext,testCaseContext,commandResultList);
