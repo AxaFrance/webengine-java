f6adf25da49566b81431fc8ae23ac18029728109
A247ZG <joseph.arul@axa.fr>
2023-10-04 15:49:15 +0200

    delete no use variable

--- webengine-drive-by-excel/src/main/java/fr/axa/automation/webengine/cmd/SwitchFrameCommand.java
+++ webengine-drive-by-excel/src/main/java/fr/axa/automation/webengine/cmd/SwitchFrameCommand.java
@@ -2,7 +2,6 @@ package fr.axa.automation.webengine.cmd;
 
 import fr.axa.automation.webengine.global.AbstractGlobalApplicationContext;
 import fr.axa.automation.webengine.global.AbstractTestCaseContext;
-import fr.axa.automation.webengine.global.TestCaseNoCodeContext;
 import fr.axa.automation.webengine.object.CommandDataNoCode;
 import fr.axa.automation.webengine.object.CommandResult;
 import org.openqa.selenium.WebDriver;
@@ -15,7 +14,6 @@ public class SwitchFrameCommand extends AbstractDriverCommand{
     @Override
     public void executeCmd(AbstractGlobalApplicationContext globalApplicationContext, AbstractTestCaseContext testCaseContext, CommandDataNoCode commandData, List<CommandResult> commandResultList) throws Exception {
         webElementDescription = populateWebElement(globalApplicationContext,testCaseContext,commandData,commandResultList);
-        String value = getValue(globalApplicationContext, (TestCaseNoCodeContext) testCaseContext, commandData, commandResultList);
         WebDriver webDriver = getWebDriverToUse(globalApplicationContext,testCaseContext,commandResultList);
         if (webDriver != null) {
             WebElement webElement = webElementDescription.findElement();
