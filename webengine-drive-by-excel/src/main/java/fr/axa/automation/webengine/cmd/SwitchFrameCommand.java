34bb0f9886102517ca8d6586abc14dfc03dc6e5c
A247ZG <joseph.arul@axa.fr>
2023-10-04 15:24:38 +0200

    Fix bug iframe

--- webengine-drive-by-excel/src/main/java/fr/axa/automation/webengine/cmd/SwitchFrameCommand.java
+++ webengine-drive-by-excel/src/main/java/fr/axa/automation/webengine/cmd/SwitchFrameCommand.java
@@ -2,10 +2,10 @@ package fr.axa.automation.webengine.cmd;
 
 import fr.axa.automation.webengine.global.AbstractGlobalApplicationContext;
 import fr.axa.automation.webengine.global.AbstractTestCaseContext;
-import fr.axa.automation.webengine.global.TestCaseNoCodeContext;
 import fr.axa.automation.webengine.object.CommandDataNoCode;
 import fr.axa.automation.webengine.object.CommandResult;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
@@ -13,10 +13,11 @@ public class SwitchFrameCommand extends AbstractDriverCommand{
 
     @Override
     public void executeCmd(AbstractGlobalApplicationContext globalApplicationContext, AbstractTestCaseContext testCaseContext, CommandDataNoCode commandData, List<CommandResult> commandResultList) throws Exception {
-        String value = getValue(globalApplicationContext, (TestCaseNoCodeContext) testCaseContext, commandData, commandResultList);
+        webElementDescription = populateWebElement(globalApplicationContext,testCaseContext,commandData,commandResultList);
         WebDriver webDriver = getWebDriverToUse(globalApplicationContext,testCaseContext,commandResultList);
         if (webDriver != null) {
-            webDriver.switchTo().frame(value);
+            WebElement webElement = webElementDescription.findElement();
+            webDriver.switchTo().frame(webElement);
         }
     }
 }
