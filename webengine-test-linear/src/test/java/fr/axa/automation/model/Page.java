923df6cdbb3e0e2e4df60456ebe89cf21a48bb52
A247ZG <joseph.arul@axa.fr>
2023-10-06 11:15:36 +0200

    Refactoring test linear project

--- webengine-test-linear/src/test/java/fr/axa/automation/model/Page.java
+++ webengine-test-linear/src/test/java/fr/axa/automation/model/Page.java
@@ -10,19 +10,14 @@ import org.openqa.selenium.WebDriver;
 @FieldDefaults(level = AccessLevel.PUBLIC)
 public class Page extends AbstractPageModel {
 
-    @Getter
     WebElementDescription language = WebElementDescription.builder().tagName("select").id("language").build();
 
-    @Getter
     WebElementDescription coffeeRadio = WebElementDescription.builder().tagName("input").id("coffee").build();
 
-    @Getter
     WebElementDescription teaRadio = WebElementDescription.builder().tagName("input").id("tea").build();
 
-    @Getter
     WebElementDescription waterRadio = WebElementDescription.builder().tagName("input").id("water").build();
 
-    @Getter
     WebElementDescription nextStep = WebElementDescription.builder().tagName("button").xPath(".//button[contains(text(),\"Next (3-second-delay)\")]").build();
 
     public Page(WebDriver webDriver) throws Exception {
