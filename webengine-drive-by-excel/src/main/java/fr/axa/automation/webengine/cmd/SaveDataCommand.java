6bb2675a7a715589933dce85ce9dcd557e87e247
A247ZG <joseph.arul@axa.fr>
2024-06-04 10:21:24 +0200

    Fix bug, save empty value
    (cherry picked from commit 088033f11d5a72e90f74185f283060d60680a96a)

--- webengine-drive-by-excel/src/main/java/fr/axa/automation/webengine/cmd/SaveDataCommand.java
+++ webengine-drive-by-excel/src/main/java/fr/axa/automation/webengine/cmd/SaveDataCommand.java
@@ -6,7 +6,9 @@ import fr.axa.automation.webengine.global.TestCaseNoCodeContext;
 import fr.axa.automation.webengine.helper.VariableHelper;
 import fr.axa.automation.webengine.object.CommandDataNoCode;
 import fr.axa.automation.webengine.object.CommandResult;
+import fr.axa.automation.webengine.util.StringUtil;
 import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
@@ -16,6 +18,9 @@ public class SaveDataCommand extends AbstractDriverCommand{
     public void executeCmd(AbstractGlobalApplicationContext globalApplicationContext, AbstractTestCaseContext testCaseContext, CommandDataNoCode commandData, List<CommandResult> commandResultList)throws Exception{
         webElementDescription = populateWebElement(globalApplicationContext,testCaseContext,commandData,commandResultList);
         String dataToSave = getValue(globalApplicationContext,(TestCaseNoCodeContext) testCaseContext, commandData, commandResultList);
+        if(StringUtils.isEmpty(dataToSave)){
+            dataToSave = StringUtil.EMPTY;
+        }
         if(MapUtils.isNotEmpty(commandData.getTargetList())){
             dataToSave = webElementDescription.getTextByElement();
             getLogReport().getVariables().add(VariableHelper.getVariable("Value in element", dataToSave));
--- webengine-drive-by-excel/src/main/java/fr/axa/automation/webengine/context/SharedNoCodeContext.java
+++ webengine-drive-by-excel/src/main/java/fr/axa/automation/webengine/context/SharedNoCodeContext.java
@@ -1,5 +1,8 @@
 package fr.axa.automation.webengine.context;
 
+import fr.axa.automation.webengine.util.StringUtil;
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -7,6 +10,10 @@ public class SharedNoCodeContext {
     public static final Map<String,String> ADDITIONAL_DATA = new ConcurrentHashMap<>();
 
     public static void addAdditionalData(String key, String information){
-        SharedNoCodeContext.ADDITIONAL_DATA.put(key,information);
+        String value = information;
+        if(StringUtils.isEmpty(value)){
+            value = StringUtil.EMPTY;
+        }
+        SharedNoCodeContext.ADDITIONAL_DATA.put(key,value);
     }
 }
